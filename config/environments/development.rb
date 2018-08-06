eaddedc185f6eaba55a82915ae8a34e436c42680
ashutosh <amxdp18@gmail.com>
2018-08-06 22:58:56 +0530

    mailer id change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            "stock2018manage@gmail.com",
-  password:             "SRMUNIV2018",
+  user_name:            "mail.repairbuck@gmail.com",
+  password:             "dgtwa34892",
   authentication:       :plain,
   enable_starttls_auto: true
 }
